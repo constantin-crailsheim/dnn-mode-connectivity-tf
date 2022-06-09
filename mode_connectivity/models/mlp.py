e302f7121c3708f2146cf9c95f9b4c920e17146f
constantin-crailsheim <constantin@vcrailsheim.de>
2022-06-09 13:01:13 +0000

    MLP added

---
+++ mode_connectivity/models/mlp.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+__all__ = [
+    'MLP',
+]
+
+class MLPBase(): # Inherit equivalent of torch.nn
+
+    def __init__(self, num_classes):
+        super(MLPBase, self).__init__()
+        self.fc_part = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(256, activation='relu'),
+            tf.keras.layers.Dense(10)])
+
+    def forward(self, x):
+        x = self.fc_part(x)
+        return x
+
+class MLPCurve(): # Inherit equivalent of torch.nn
+
+    def __init__(self, num_classes, fix_points):
+        super(MLPCurve, self).__init__()
+        
+
+    def forward(self, x, coeffs_t):
+        return x
+
+
+class MLP:
+    base = MLPBase
+    curve = MLPCurve
+    kwargs = {}
+
+
