90fb11e0b44594dfc7b3ee637870eed0fe5a0880
AmelieLiese <amelie.loessl@campus.lmu.de>
2022-06-11 16:03:12 +0000

    Es fehlt noch: if-else Bedingung noch vervollständigen, Größe von train, test und val set bestimmen

--- mode_connectivity/data.py
+++ mode_connectivity/data.py
@@ -1,16 +1,83 @@
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras import layers
+#from tensorflow import keras
+#from keras import layers
 
 class Transforms:
-    pass
 
-    class MNIST:
-        pass
+    class mnist:
 
-    class RegressionData:
-        pass
+        class CNN:
+
+            train = lambda X, y: (
+                tf.image.random_flip_left_right(X, seed = global_seed),
+                tf.image.random_crop(X, size = (24,24), seed = global_seed), # global_seed not specified yet. See issue #2
+                tf.image.resize_with_crop_or_pad(X, target_height = 28, target_width= 28),
+                tf.image.per_image_standardization(X)
+            )
+
+            test = lambda X, y: (
+                tf.image.per_image_standardization(X)
+            )
+            
+            
+            #train = tf.keras.Sequential([
+            #    #keras.Input(),
+            #    layers.RandomFlip(mode = "horizontal", seed = 1),  #global_seed -> dummy variable. Not specified yet. See issue #2
+            #    layers.RandomCrop(height = 32, width = 32, seed = 1),
+            #    layers.ZeroPadding2D(padding = (4,4)),
+            #    layers.Normalization(axis = None, mean = [0.485, 0.456, 0.406], variance = [pow(0.229,2), pow(0.224,2), pow(0.225,2)])
+            #])
+
+            #test = tf.keras.Sequential([
+            #    #keras.Input(),
+            #    layers.Normalization(axis = None, mean = [0.485, 0.456, 0.406], variance = [pow(0.229,2), pow(0.224,2), pow(0.225,2)])
+            #])
+        
+        class MLP:
+
+            train = lambda X, y: (
+                tf.image.random_flip_left_right(X, seed = global_seed),
+                tf.image.random_crop(X, size = (24,24), seed = global_seed), # global_seed not specified yet. See issue #2
+                tf.image.resize_with_crop_or_pad(X, target_height = 28, target_width= 28),
+                tf.image.per_image_standardization(X)
+                )
+
+            test = lambda X, y: (
+                tf.image.per_image_standardization(X)
+                )
+            
+            #train = tf.keras.Sequential([
+            #    #keras.Input(),
+            #    layers.RandomFlip(mode = "horizontal", seed = 1),  #global_seed -> dummy variable. Not specified yet. See issue #2
+            #    layers.RandomCrop(height = 32, width = 32, seed = 1),
+            #    layers.ZeroPadding2D(padding = (4,4)),
+            #    layers.Normalization(axis = None, mean = [0.485, 0.456, 0.406], variance = [pow(0.229,2), pow(0.224,2), pow(0.225,2)])
+            #])
+
+            #test = tf.keras.Sequential([
+            #    #keras.Input(),
+            #    layers.Normalization(axis = None, mean = [0.485, 0.456, 0.406], variance = [pow(0.229,2), pow(0.224,2), pow(0.225,2)])
+            #])
+
+   # class RegressionData:
+       
+        # class CNN:
+           
+            #train = 
+
+            #test = 
+
+       # class MLP:  
+           
+            #train = 
+
+            #test =   
+
+
+    
 
 
 def loaders(
@@ -22,4 +89,67 @@ def loaders(
     use_test: bool = False,
     shuffle_train: bool = True,
 ):
-    pass
+    #ds = getattr(keras.datasets, dataset)
+    path = os.path.join(path, dataset.lower())
+    transform = getattr(getattr(Transforms, dataset), transform_name)
+    #train_set = ds(path, train = True, download = True, transform  = transform.train)
+
+    (train_set, test_set), ds_info = tfds.load(
+    dataset,
+    split=['train', 'test'],
+    shuffle_files=shuffle_train,
+    as_supervised=True,
+    with_info=True,
+    )
+
+    process = lambda X, y: (tf.expand_dims(X, axis=3) / 255,
+                            tf.cast(y, dtype='int32'))
+
+    if use_test:
+        print('You are going to tun models on the test set. Are you sure?')
+        test_set = 
+        pass
+
+    else:
+        print("Using train (45000) + validation (5000)")
+        
+        pass
+
+    return{
+        'train': tf.data.Dataset.from_tensor_slices(
+            process(*train_set)).batch(batch_size).shuffle(len(train_set[0])).map(transform.train),
+        'test': tf.data.Dataset.from_tensor_slices(
+            process(*test_set)).batch(batch_size).map(transform.test)
+        }, max(train_set.train_labels) +1
+
+
+
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow import keras
+from keras import layers
+dataset="mnist"
+shuffle_train = True
+
+(train_set, test_set), ds = tfds.load(
+    dataset,
+    split=['train', 'test'],
+    shuffle_files=shuffle_train,
+    as_supervised=True,
+    with_info=True,
+    )
+
+print(ds)    
+
+#dataset = keras.datasets.mnist
+#(train_images_tf, train_labels_tf), (test_images_tf, test_labels_tf) = dataset.load_data()
+#train_images_tf = train_images_tf / 255.0
+#test_images_tf = test_images_tf / 255.0
+
+#plt.imshow(train_images_tf[1])
+
+# %%
