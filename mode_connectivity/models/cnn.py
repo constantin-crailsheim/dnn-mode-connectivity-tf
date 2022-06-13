280183e627e8f4c8f5d759b5d9c1146ba1b83585
davidrundel <david.rundel@campus.lmu.de>
2022-06-13 09:02:45 +0200

    Implemented proposed changes to Pull Request

--- mode_connectivity/models/cnn.py
+++ mode_connectivity/models/cnn.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from typing import List
 
 __all__ = [
     "CNN",
@@ -6,6 +7,10 @@ __all__ = [
 
 
 class CNNBase(tf.keras.Model):
+    num_classes: int
+    conv_part: tf.keras.Sequential
+    fc_part: tf.keras.Sequential
+
     #Sources:   
     #https://www.tensorflow.org/tutorials/customization/custom_layers
     #https://www.tensorflow.org/api_docs/python/tf/keras/layers
@@ -18,8 +23,8 @@ class CNNBase(tf.keras.Model):
 
     # Comment: In contrast to PyTorch there are no input dimensions required in Tensorflow.
 
-    def __init__(self, num_classes):
-        super(CNNBase, self).__init__()
+    def __init__(self, num_classes: int):
+        super().__init__()
         self.num_classes = num_classes
 
         self.conv_part = tf.keras.Sequential([
@@ -35,20 +40,19 @@ class CNNBase(tf.keras.Model):
             tf.keras.layers.Dense(units= 64, activation='relu'),
             tf.keras.layers.Dense(units= self.num_classes)]) 
 
-    def call(self, x):
-        # Forward compuation (Equivalent to forward() in PyTorch)
-        return self.conv_part(self.fc_part(x))
+    def call(self, inputs, training=None, mask=None): #TO DO: Typehints & Chech which arguments necessary
+        return self.conv_part(self.fc_part(inputs))
+        #TO DO: Check if x = x.view(x.size(0), -1) necessary 
         
 
-class CNNCurve:  # Inherit equivalent of torch.nn
-    def __init__(self, num_classes, fix_points):
-        super(CNNCurve, self).__init__()
+class CNNCurve(tf.keras.Model):
+    def __init__(self, num_classes: int, fix_points: List[bool]):
+        super().__init__()
         self.num_classes = num_classes
         self.fix_points = fix_points
-
         # TO DO
 
-    def forward(self, x, coeffs_t):
+    def call(self, inputs, coeffs_t, training=None, mask=None): #TO DO: Typehints
         # TO DO
         pass
 
