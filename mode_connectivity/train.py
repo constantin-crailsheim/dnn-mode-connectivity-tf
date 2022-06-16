711a3be112216d3535d0942743aced7bb808f336
davidrundel <david.rundel@campus.lmu.de>
2022-06-16 13:24:47 +0200

    Implemented CurveNet initialization in get_model()

--- mode_connectivity/train.py
+++ mode_connectivity/train.py
@@ -19,6 +19,7 @@ from utils import (
     load_checkpoint,
     save_checkpoint,
 )
+import curves
 
 COLUMNS = ["ep", "lr", "tr_loss", "tr_acc", "te_nll", "te_acc", "time"]
 
@@ -39,10 +40,9 @@ def main():
     )
     architecture = get_architecture(model_name=args.model)
     model = get_model(
-        curve=args.curve,
-        architecture=architecture,
-        num_classes=num_classes,
-        weight_decay=args.wd,
+        architecture = architecture,
+        args = args,
+        num_classes = num_classes,
     )
 
     # criterion = tf.nn.sparse_softmax_cross_entropy_with_logits
@@ -96,14 +96,49 @@ def get_architecture(model_name: str):
     raise KeyError(f"Unkown model {model_name}")
 
 
-def get_model(curve: str, architecture, num_classes: int, weight_decay: float):
-    if not curve:
-        return architecture.base(
-            num_classes=num_classes, weight_decay=weight_decay, **architecture.kwargs
+def get_model(architecture, args, num_classes: int): 
+    #Changed method arguments to take args as input () since many of those variables needed in curve-case
+
+    #If no curve is to be fit the base version of the architecture is initialised (e.g CNNBase instead of CNNCurve).
+    if not args.curve:
+        model = architecture.base(num_classes=num_classes, weight_decay=weight_decay, **architecture.kwargs)
+        return model
+
+    #Otherwise the curve version of the architecture (e.g. CNNCurve) is initialised in the context of a CurveNet.
+    #The CurveNet additionally contains the curve (e.g. Bezier) and imports the parameters of the pre-trained base-nets that constitute the outer points of the curve.
+    else:
+        curve = getattr(curves, args.curve)
+        model = curves.CurveNet(
+            num_classes,
+            curve,
+            architecture.curve,
+            args.num_bends,
+            args.fix_start,
+            args.fix_end,
+            architecture_kwargs=architecture.kwargs,
         )
 
-    # TODO return curve model
-    return None
+        base_model = None
+        if args.resume is None:
+            for path, k in [(args.init_start, 0), (args.init_end, args.num_bends - 1)]:
+                if path is not None:
+                    if base_model is None:
+                        base_model = architecture.base(num_classes=num_classes, **architecture.kwargs)
+                    print('Loading %s as point #%d' % (path, k))
+
+                    # Pytorch  Version:
+                    # checkpoint = torch.load(path) #Angepasst                    
+                    # base_model.load_state_dict(checkpoint['model_state']) #Angepasst
+
+                    # Source of TF Translation: https://d2l.ai/chapter_deep-learning-computation/read-write.html
+
+                    base_model.load_weights(path)
+                    model.import_base_parameters(base_model, k)
+            if args.init_linear:
+                print('Linear initialization.')
+                model.init_linear()
+                
+        return model
 
 
 def train(
