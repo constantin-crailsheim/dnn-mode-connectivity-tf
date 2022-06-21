e9f44432cbaf7e75d61393a8da52905ba4347de4
Fabian Stermann <fabian.stermann@gmail.com>
2022-06-21 14:50:59 +0000

    #44 Correctly load in base params and init_linear

--- mode_connectivity/curves.py
+++ mode_connectivity/curves.py
@@ -1,3 +1,5 @@
+import logging
+import re
 from abc import ABC, abstractmethod
 from typing import Any, Dict, List, Tuple, Type, Union
 
@@ -5,7 +7,8 @@ import numpy as np
 import tensorflow as tf
 from scipy.special import binom
 
-from .utils import split_list
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
 class Curve(tf.keras.layers.Layer, ABC):
@@ -53,11 +56,13 @@ class CurveNet(tf.keras.Model):
 
     curve: Curve
     curve_model: tf.keras.Model
+    curve_layers: List[tf.keras.layers.Layer]  # CurveLayer
 
     def __init__(
         self,
         num_classes: int,
         num_bends: int,
+        weight_decay: float,
         curve: Type[Curve],  # Bezier, Polychain
         curve_model: Type[tf.keras.Model],  # ConvFCCurve, VGGCurve
         fix_start: bool = True,
@@ -72,41 +77,105 @@ class CurveNet(tf.keras.Model):
 
         self.curve = curve(self.num_bends)
         self.curve_model = curve_model(
-            num_classes=num_classes, fix_points=self.fix_points, **architecture_kwargs
+            num_classes=num_classes,
+            fix_points=self.fix_points,
+            weight_decay=weight_decay,
+            **architecture_kwargs,
         )
         self.curve_layers = [
             layer
-            for layer in self.curve_model.submodules()  # TODO Need to check if this gathers the correct modules
+            for layer in self.curve_model.submodules
             if issubclass(layer.__class__, CurveLayer)
         ]
 
     def import_base_parameters(self, base_model: tf.keras.Model, index: int) -> None:
         """Import parameters from the base model into this model.
 
+        Parameters in the Base Model (without Curve layers) are
+        saved with the name
+            <layerName>_<layerIndex>/<parameter>:0
+
+        For example:
+            conv2d/kernel:0
+            conv2d/bias:0
+            dense_1/kernel:0
+
+        Whereas parameters in the Curve Model are save with a '_curve' suffix
+        and an index that matches them to the curve point.
+
+        For example:
+            conv2d_curve/kernel_curve_1:0
+            conv2d_curve/bias_curve_0:0
+            dense_1_curve/kernel_curve_2:0
+
+        Here we want to load weights for a specific point on the curve
+        from the pretrained Base Model.
+
         Args:
             base_model (tf.keras.Model): The base model from which parameters should be imported.
-            index (int): _description_
+            index (int): Index of the curve point.
         """
-        variables: List[tf.Variable] = self.curve_model.variables[
-            index :: self.num_bends
-        ]
-        base_variables: List[tf.Variable] = base_model.variables
+        if not self.curve_model.built:
+            self._build_from_base_model(base_model)
 
-        # or should we call get/set_weights ?
-        # would be even better to initialize by names
-        # the indexing [index :: self.num_bends] above is not so clear
-        for variable, base_variable in zip(variables, base_variables):
-            variable.assign(base_variable.value)
+        weights = {w.name: w for w in self.curve_model.variables}
+        base_weights = {w.name: w for w in base_model.variables}
+
+        assigned_weights = []
+        for name, weight in weights.items():
+            parameter_index = self._find_parameter_index(name)
+            if parameter_index != index:
+                # Kernel/Bias index doesn't match the curve index.
+                continue
+
+            base_name = self._get_base_name(name, index)
+            base_weight = base_weights.get(base_name)
+            if base_weight is None:
+                logger.debug(
+                    f"Could not assign to weight {name} (base_name: {base_name})"
+                )
+                continue
+
+            weight.assign(base_weight.value())
+            assigned_weights.append(f"{base_name} -> {name}")
+
+        logger.info(
+            f"Assigned weights for point #{index}: {', '.join(assigned_weights)}"
+        )
+
+    def _build_from_base_model(self, base_model: tf.keras.Model):
+        """Build the model to initialize weights."""
+        base_input_shape = base_model.layers[0].input_shape
+        coeffs_t_input_shape = (self.num_bends,)
+        input_shape = [
+            tf.TensorShape(base_input_shape),
+            tf.TensorShape(coeffs_t_input_shape),
+        ]
+        self.curve_model.build(input_shape)
+
+    @staticmethod
+    def _find_parameter_index(parameter_name: str) -> Union[int, None]:
+        """Finds the index (respective curve point) of a curve parameter name."""
+        results = re.findall(r"(\d+):", parameter_name)
+        if not results:
+            return None
+        return int(results[0])
+
+    @staticmethod
+    def _get_base_name(parameter_name: str, index: int):
+        """Returns the base model parameter name, given a curve model parameter name."""
+        return parameter_name.replace("_curve", "").replace(f"_{index}:", ":")
 
     def init_linear(self) -> None:
-        """Initialize the linear layer of the model."""
-        split_weights = split_list(self.curve_model.variables, size=self.num_bends)
-        for weights in split_weights:
-            self._compute_inner_weights(weights=weights)
+        """Initialize the linear inner curve weights of the model."""
+        # TODO What does 'init_linear' mean? This does not initialize a linear layer.
+        for layer in self.curve_layers:
+            self._compute_inner_weights(weights=layer.curve_kernels)
+            self._compute_inner_weights(weights=layer.curve_biases)
 
     def _compute_inner_weights(self, weights: List[tf.Variable]) -> None:
         # Is this procedure mentioned somewhere in the paper?
-        first_weight, last_weight = weights[0].value, weights[-1].value
+        first_weight, last_weight = weights[0].value(), weights[-1].value()
         for i in range(1, self.num_bends - 1):
             alpha = i * 1.0 / (self.num_bends - 1)
             weights[i].assign(alpha * first_weight + (1.0 - alpha) * last_weight)
@@ -127,7 +196,7 @@ class CurveNet(tf.keras.Model):
         if uniform_tensor is None:
             uniform_tensor = tf.random.uniform(shape=(1,), dtype=inputs.dtype)
         coeffs_t = self.curve(uniform_tensor)
-        output = self.curve_model(inputs, coeffs_t)
+        output = self.curve_model((inputs, coeffs_t))
         self._compute_l2()
         return output
 
@@ -154,6 +223,14 @@ class CurveLayer(tf.keras.layers.Layer, ABC):
         self.fix_points = fix_points
         self.num_bends = len(self.fix_points)
         self.l2 = 0.0
+        self._reset_input_spec()
+
+    def _reset_input_spec(self):
+        """Modify the input specification to take in the curve coefficients as well."""
+        self.input_spec = [
+            self.input_spec,
+            tf.keras.layers.InputSpec(shape=((self.num_bends,))),
+        ]
 
     @abstractmethod
     def build(self, *args, **kwargs):
@@ -177,8 +254,8 @@ class CurveLayer(tf.keras.layers.Layer, ABC):
             kernel_shape (Tuple): Shape of the kernel.
             bias_shape (Tuple): Shape of the bias.
         """
-        self.curve_kernels = {}
-        self.curve_biases = {}
+        self.curve_kernels = [None] * len(self.fix_points)
+        self.curve_biases = [None] * len(self.fix_points)
         for i, fixed in enumerate(self.fix_points):
             self.curve_kernels[i] = self._add_kernel(i, kernel_shape, fixed)
             if self.use_bias:
@@ -191,7 +268,7 @@ class CurveLayer(tf.keras.layers.Layer, ABC):
         return self._add_weights(index, shape, fixed, "bias")
 
     def _add_weights(self, index: int, shape: Tuple, fixed: bool, parameter_type: str):
-        name = f"curve_{parameter_type}_{index}"
+        name = f"{parameter_type}_curve_{index}"
         weight = self.add_weight(
             name=name,
             shape=shape,
@@ -245,8 +322,8 @@ class CurveLayer(tf.keras.layers.Layer, ABC):
 
         # I think this could also be solved by matrix multiplication.
         weight_sum = 0
-        for i, coeff in enumerate(coeffs_t):
-            weight_sum += weights[i].value() * coeff
+        for i in range(len(coeffs_t)):
+            weight_sum += weights[i].value() * coeffs_t[i]
 
         if weight_sum is not None:
             # I think this should always be called
@@ -270,19 +347,17 @@ class Conv2DCurve(CurveLayer, tf.keras.layers.Conv2D):
         )
 
     def build(self, input_shape):
-        tf.keras.layers.Conv2D.build(self, input_shape)
-        # Built gets called once when call() is called for the first time.
-        input_shape = tf.TensorShape(input_shape)
-        input_channel = self._get_input_channel(input_shape)
-        kernel_shape = self.kernel_size + (input_channel // self.groups, self.filters)
-        bias_shape = (self.filters,)
-
+        tf.keras.layers.Conv2D.build(self, input_shape[0])
+        self._reset_input_spec()
         # Register curve kernels and biases
-        self.add_parameter_weights(kernel_shape=kernel_shape, bias_shape=bias_shape)
+        self.add_parameter_weights(
+            kernel_shape=self.kernel.shape, bias_shape=self.bias.shape
+        )
 
-    def call(self, inputs, coeffs_t: tf.Tensor):
+    def call(self, inputs: Tuple[tf.Tensor, tf.Tensor]):
+        x, coeffs_t = inputs
         self.kernel, self.bias = self.compute_weights_t(coeffs_t)
-        return tf.keras.layers.Conv2D.call(self, inputs)
+        return tf.keras.layers.Conv2D.call(self, x)
 
 
 class DenseCurve(CurveLayer, tf.keras.layers.Dense):
@@ -295,23 +370,14 @@ class DenseCurve(CurveLayer, tf.keras.layers.Dense):
         super().__init__(units=units, fix_points=fix_points, **kwargs)
 
     def build(self, input_shape):
-        tf.keras.layers.Dense.build(self, input_shape)
-        input_shape = tf.TensorShape(input_shape)
-        last_dim = tf.compat.dimension_value(input_shape[-1])
-        kernel_shape = [last_dim, self.units]
-        bias_shape = [
-            self.units,
-        ]
+        tf.keras.layers.Dense.build(self, input_shape[0])
+        self._reset_input_spec()
+        # Register curve kernels and biases
+        self.add_parameter_weights(
+            kernel_shape=self.kernel.shape, bias_shape=self.bias.shape
+        )
 
-        if last_dim is None:
-            raise ValueError(
-                "The last dimension of the inputs to a Dense layer "
-                "should be defined. Found None. "
-                f"Full input shape received: {input_shape}"
-            )
-
-        self.add_parameter_weights(kernel_shape=kernel_shape, bias_shape=bias_shape)
-
-    def call(self, inputs, coeffs_t: tf.Tensor):
+    def call(self, inputs: Tuple[tf.Tensor, tf.Tensor]):
+        x, coeffs_t = inputs
         self.kernel, self.bias = self.compute_weights_t(coeffs_t)
-        return tf.keras.layers.Dense(self, inputs)
+        return tf.keras.layers.Dense.call(self, x)
