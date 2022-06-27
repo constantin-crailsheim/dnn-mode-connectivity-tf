0e03de87c0a906f3240d30e9f9cfa2fe86e83844
davidrundel <david.rundel@campus.lmu.de>
2022-06-27 19:02:53 +0200

    Implementation with pytest.fixtures

--- tests/test_CurveLayer.py
+++ tests/test_CurveLayer.py
@@ -11,34 +11,35 @@ import pytest
 class CurveLayerTest:
     fix_points = [True, False, True]
 
-    @abstractmethod
-    def init_standard_layer(self, *args, **kwargs):
-        pass
 
-    @abstractmethod
-    def call_standard_layer(self, *args, **kwargs):
-        pass
-
-    def get_curve_point_weights(self):
+    @pytest.fixture
+    def curve_point_weights(self):
         num_bends= len(self.fix_points)
         bezier_curve= Bezier(num_bends)
         rand_t= tf.random.uniform(shape=(1,))
         return bezier_curve(rand_t)
+
+    @abstractmethod
+    @pytest.fixture
+    def initialized_layer(self, *args, **kwargs):
+        pass
+
+    @pytest.fixture
+    def built_layer(self, initialized_layer, curve_point_weights):
+        output= initialized_layer([tf.random.uniform(shape=self.input_shape), curve_point_weights])
+        return initialized_layer
+
+    def call_layer(self, layer, curve_point_weights= curve_point_weights):
+        #Attention: We do not want a new layer here, hence we call the variable layer and not as one of the fixtures
+        output= layer([tf.random.uniform(shape=self.input_shape), curve_point_weights])
+        return output, curve_point_weights
 
 
     @abstractmethod
     def test_init(self, *args, **kwargs):
         pass
 
-
-    def test_build(self):
-        layer = self.init_standard_layer()
-        #Also calls build()
-        self.call_standard_layer(layer)
-        
-        self.built_layer_test(layer) 
-
-    def built_layer_test(self, built_layer):
+    def test_build(self, built_layer):
         # Build in parent class tf.keras.layers... should add kernel and bias parameters 
         # (Even though they won't be used directly)        
         assert built_layer.kernel != None
@@ -58,59 +59,48 @@ class CurveLayerTest:
                 if param != "bias":
                     assert tf.math.count_nonzero(temp_param[i]) != 0 #Check if initialized
 
-    def test_call(self):
-        layer = self.init_standard_layer()
-        self.param_updates_test(layer)
-
-        output, curve_point_weights= self.call_standard_layer(layer)
-        self.check_output_size(output)
-
-    def param_updates_test(self, layer):
-        # Even without optimization of the network parameters, the params should be updated
-        # since we change the location on the curve/ randomly set curve_point_weights of the submodels.
-        self.call_standard_layer(layer)
-        old_kernel= layer.kernel
-        old_bias= layer.bias
-        self.call_standard_layer(layer)
-        new_kernel= layer.kernel
-        new_bias= layer.bias
+    def test_call(self, built_layer):
+        # Even without optimization of the networks parameters, the params should be updated since we change the location on the curve.
+        old_kernel= built_layer.kernel
+        old_bias= built_layer.bias
+        output, curve_point_weights= self.call_layer(built_layer)
+        new_kernel= built_layer.kernel
+        new_bias= built_layer.bias
 
         assert not tf.experimental.numpy.allclose(old_kernel, new_kernel)
         assert not tf.experimental.numpy.allclose(old_bias, new_bias)
+
+        self.check_output_size(output)
 
     @abstractmethod
     def check_output_size(self, output):
         pass
 
-    def test_reset_input_spec(self):
-        #To Do
-        pass
 
-    def test_compute_weighted_parameters(self):
+    def test_compute_weighted_parameters(self, built_layer):
         #Call() calls compute_weighted_parameters()
-        layer = self.init_standard_layer()
-        output, curve_point_weights= self.call_standard_layer(layer)
+        output, curve_point_weights= self.call_layer(built_layer)
 
         #Alternative calculation of weighted params
         alt_kernel= None
         alt_bias= None
 
-        for i in range(len(layer.curve_kernels)):
-            scaled_kernel= curve_point_weights[i] * layer.curve_kernels[i]
+        for i in range(len(built_layer.curve_kernels)):
+            scaled_kernel= curve_point_weights[i] * built_layer.curve_kernels[i]
             if alt_kernel == None:
                 alt_kernel = scaled_kernel
             else:
                 alt_kernel += scaled_kernel
 
-        for i in range(len(layer.curve_biases)):
-            scaled_bias= curve_point_weights[i] * layer.curve_biases[i]
+        for i in range(len(built_layer.curve_biases)):
+            scaled_bias= curve_point_weights[i] * built_layer.curve_biases[i]
             if alt_bias == None:
                 alt_bias = scaled_bias
             else:
                 alt_bias += scaled_bias
 
-        assert tf.experimental.numpy.allclose(layer.kernel, alt_kernel)
-        assert tf.experimental.numpy.allclose(layer.bias, alt_bias)
+        assert tf.experimental.numpy.allclose(built_layer.kernel, alt_kernel)
+        assert tf.experimental.numpy.allclose(built_layer.bias, alt_bias)
 
     # Still necessary if we have test_init?
     # def test_init_direct(self): 
@@ -126,44 +116,34 @@ class TestConv2DCurveLayer(CurveLayerTest):
     kernel_size=(3, 3)
     input_shape= (128, 28, 28, 1)
 
-    def init_standard_layer(self):
+    @pytest.fixture
+    def initialized_layer(self):
         return Conv2DCurve(self.filters, self.kernel_size, self.fix_points)
 
-    def call_standard_layer(self, conv2d_curve_layer):
-        curve_point_weights= self.get_curve_point_weights()
-        output= conv2d_curve_layer([tf.random.uniform(shape=self.input_shape), curve_point_weights])
-        return output, curve_point_weights
-
-    def test_init(self):    
-        conv2d_curve_layer = self.init_standard_layer()
-        assert isinstance(conv2d_curve_layer, CurveLayer)
-        assert isinstance(conv2d_curve_layer, tf.keras.layers.Conv2D)
+    def test_init(self, initialized_layer):    
+        assert isinstance(initialized_layer, CurveLayer)
+        assert isinstance(initialized_layer, tf.keras.layers.Conv2D)
 
     def check_output_size(self, output):
         output_shape_h = self.input_shape[1] - self.kernel_size[0] + 1
         output_shape_w = self.input_shape[2] - self.kernel_size[1] + 1
-        assert output.shape == (128, output_shape_h, output_shape_w, 32)
+        assert output.shape == (self.input_shape[0], output_shape_h, output_shape_w, self.filters)
 
 
 class TestDenseCurveLayer(CurveLayerTest):
     units = 16
     input_shape= (128, 32)  
 
-    def init_standard_layer(self):
+    @pytest.fixture
+    def initialized_layer(self):
         return DenseCurve(self.units, self.fix_points)
 
-    def call_standard_layer(self, dense_curve_layer):
-        curve_point_weights= self.get_curve_point_weights()
-        output= dense_curve_layer([tf.random.uniform(shape= self.input_shape), curve_point_weights])
-        return output, curve_point_weights
-
-    def test_init(self):
-        dense_curve_layer = self.init_standard_layer()
-        assert isinstance(dense_curve_layer, CurveLayer)
-        assert isinstance(dense_curve_layer, tf.keras.layers.Dense)   
+    def test_init(self, initialized_layer):
+        assert isinstance(initialized_layer, CurveLayer)
+        assert isinstance(initialized_layer, tf.keras.layers.Dense)   
 
     def check_output_size(self, output):
-        assert output.shape == (self.input_shape[:-1][0], self.units)
+        assert output.shape == (self.input_shape[0], self.units)
 
 
 # If we add regularizer, test whether correct kernel is optimized.
