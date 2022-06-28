5468e89b509495a251e5ad7097748f8be4f191d9
AmelieLiese <amelie.loessl@campus.lmu.de>
2022-06-28 09:48:08 +0000

    add load_model function

--- mode_connectivity/plot_functions.py
+++ mode_connectivity/plot_functions.py
@@ -7,6 +7,9 @@ import tensorflow as tf
 from keras.layers import Layer
 from keras.optimizers import Optimizer
 
+from argparser import Arguments, parse_evaluate_arguments
+from data import data_loaders
+
 import mode_connectivity.curves.curves as curves
 from mode_connectivity.train import test_epoch
 from mode_connectivity.train import test_batch
@@ -26,6 +29,90 @@ from mode_connectivity.utils import (
     save_weights,
 )
 
+from utils import (
+    adjust_learning_rate,
+    check_batch_normalization,
+    l2_regularizer,
+    learning_rate_schedule,
+    load_checkpoint,
+    save_checkpoint,
+    save_model,
+    save_weights,
+)
+
+
+def load_model_and_save_plots(model: str):
+    args = parse_evaluate_arguments()
+
+    loaders, num_classes, n_datasets = data_loaders(
+        dataset=args.dataset,
+        path=args.data_path,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        transform_name=args.transform,
+        use_test=args.use_test,
+    )
+    
+    architecture = get_architecture(model_name=args.model)
+    model = load_model(
+        architecture=architecture,
+        args=args,
+        num_classes=num_classes,
+        input_shape=(None, 28, 28, 1),
+    )
+
+    test_predictions = prediction_epoch(
+                test_loader = loaders["test"],
+                model = model,
+                criterion = criterion,
+                n_test = n_datasets["test"],
+                point_on_curve = point_on_curve_tensor,
+                regularizer = regularizer
+            )
+
+    pred = test_predictions['pred'] 
+    output = test_predictions['output']  
+    target = test_predictions['target'] 
+
+    print_and_save_model_predictions_and_plots(pred, output, target, dir = args.dir, save: bool = True)
+
+def load_model(architecture, args: Arguments, num_classes: int, input_shape):
+    # Changed method arguments to take args as input () since many of those variables needed in curve-case
+
+    # If no curve is to be fit the base version of the architecture is initialised (e.g CNNBase instead of CNNCurve).
+    if not args.curve:
+        model = architecture.base(
+            num_classes=num_classes, weight_decay=args.wd, **architecture.kwargs
+        )
+
+        model.build(input_shape=input_shape)
+        model.load_weights(filepath=args.ckpt)
+        model.compile()
+
+        return model
+
+    # Otherwise the curve version of the architecture (e.g. CNNCurve) is initialised in the context of a CurveNet.
+    # The CurveNet additionally contains the curve (e.g. Bezier) and imports the parameters of the pre-trained base-nets that constitute the outer points of the curve.
+    else:
+        curve = getattr(curves, args.curve)
+        model = CurveNet(
+            num_classes=num_classes,
+            num_bends=args.num_bends,
+            weight_decay=args.wd,
+            curve=curve,
+            curve_model=architecture.curve,
+            fix_start=args.fix_start,
+            fix_end=args.fix_end,
+            architecture_kwargs=architecture.kwargs,
+        )
+
+        model.build(input_shape=input_shape)
+        model.load_weights(filepath=args.ckpt)
+        model.compile()
+
+        return model
+
+
 def prediction_epoch(
     test_loader: Iterable,
     model: Layer,
@@ -37,7 +124,10 @@ def prediction_epoch(
     loss_sum = 0.0,
     correct = 0.0,
     **kwargs,
-):
+)-> Dict[str, tf.Tensor]:
+    pred = []
+    output = []
+    target = []
 
     # PyTorch: model.eval()
     for input, target in test_loader:
@@ -59,7 +149,12 @@ def prediction_epoch(
         output = output_batch
         target  = target_batch
 
-    print_and_save_model_predictions_and_plots(pred, output, target, dir = dir, save: bool = True)
+     return {
+        "pred": pred,  
+        "output": output,  
+        "target": target, 
+    }
+   
 
     
 
