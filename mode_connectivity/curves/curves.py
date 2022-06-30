cb6f97d66b46592b7034c050bda95f008f37a1a6
Fabian Stermann <fabian.stermann@gmail.com>
2022-06-30 09:08:34 +0000

    #79 add curve docs

--- mode_connectivity/curves/curves.py
+++ mode_connectivity/curves/curves.py
@@ -7,11 +7,21 @@ from scipy.special import binom
 
 
 class Curve(tf.keras.layers.Layer, ABC):
+    """Base class for Curve Layers.
+
+    Args:
+            degree (int): Degree of the Curve. Needs to be greater than 0.
+                1: Linear
+                2: Quadratic
+                3: Cubic
+                ...
+    """
+
     degree: int
 
     def __init__(self, degree: int):
+        """Initialize the Curve Layer."""
         super().__init__()
-
         if degree < 1:
             raise ValueError(
                 f"Degree ({degree=}) of the curve needs to be greater than 0."
@@ -27,6 +37,23 @@ class Curve(tf.keras.layers.Layer, ABC):
 
 
 class Bezier(Curve):
+    """Implementation of the Bezier Curve in a Layer context.
+
+    Args:
+            degree (int): Degree of the Curve. Needs to be greater than 0.
+                1: Linear
+                2: Quadratic
+                3: Cubic
+                ...
+
+    Calling this layer returns a Tensor of weights, which sum up to 1.
+    The weights correspond to the weight each input point of the Bezier curve is given when constructing the curve.
+    This Tensor is then used to weigh parameters of CurveLayers.
+
+    For further information, see:
+    https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+    """
+
     def __init__(self, degree: int):
         super().__init__(degree=degree)
         self.binom = tf.Variable(
@@ -37,10 +64,6 @@ class Bezier(Curve):
         self.rev_range = tf.Variable(
             tf.range(float(degree), -1, delta=-1), trainable=False
         )
-
-        # Not sure if this is the best way to substitute register_buffer() in PyTorch
-        # The PyTorch Buffer in this example is not considered a model parameter, not trained,
-        # part of the module's state, moved to cuda() or cpu() with the rest of the model's parameters
 
     def call(self, point_on_curve: Union[float, tf.Tensor]) -> tf.Tensor:
         return (
