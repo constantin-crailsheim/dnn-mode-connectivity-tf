97ad7da5ec3b2b9ab8b8790f15efa9808937f69c
davidrundel <david.rundel@campus.lmu.de>
2022-07-01 15:26:07 +0200

    Ausführbarer Zwischenstand 2

--- tests/test_CurveNet.py
+++ tests/test_CurveNet.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-from mode_connectivity.curves.curves import Bezier
+from mode_connectivity.curves.curves import Curve, Bezier
 from mode_connectivity.curves.net import CurveNet
 from mode_connectivity.models import CNN, MLP
 
@@ -45,35 +45,18 @@ class TestCurveNet:
     # Same for MLP.curve
 
     def test_init(self, initialized_curve_net):
-        temp_net = initialized_curve_net
+        assert isinstance(initialized_curve_net.curve, Curve)
+        assert isinstance(initialized_curve_net.curve_model, tf.keras.Model)
 
     def test_call(self, initialized_curve_net):
         output= initialized_curve_net(inputs= tf.random.uniform((128, 28, 28, 1)))
 
         assert output.shape == (128, 10) #(input_size[0], num_classes)
 
-    # def test_import_base_parameters(self, initialized_curve_net):
-    #     base_model= CNN.base(10)
-    #     base_model(tf.random.uniform((128, 28, 28, 1)))
-
-    #     initialized_curve_net.import_base_parameters(base_model, 0)
-
     def test_import_base_parameters(self, initialized_curve_net):
-    #def test_import_base_parameters(self):
        #TO DO: Als Fixture mit anderen Parametrisierungen, zb index letzter
         #TO DO: Noch vergleichen ob in curve die mit index anders ist als vor load
         # und alle anderen gleich wie vor laod
-
-        #Weg dann
-        # initialized_curve_net= CurveNet(
-        #     num_classes=10,
-        #     num_bends=3,
-        #     weight_decay=1e4,
-        #     curve=Bezier,
-        #     curve_model=CNN.curve,
-        #     fix_start=True,
-        #     fix_end=True,
-        #     architecture_kwargs=CNN.kwargs)
 
         curve_net = initialized_curve_net
 
@@ -82,11 +65,9 @@ class TestCurveNet:
 
         curve_net._build_from_base_model(base_model)
 
-        temp_var= curve_net.curve_model.variables
-
 
         #curve_weights_old = {w.name.split("/", 2)[2]: w for w in curve_net.curve_model.variables}
-        curve_weights_old = {w.name: tf.Variable(w) for w in curve_net.curve_model.variables}
+        curve_weights_old = {w.name: tf.Variable(w) for w in curve_net.curve_model.variables} #tf.Variable creates a fixed copy
 
         #copy, remove
 
@@ -107,53 +88,63 @@ class TestCurveNet:
                 if base_param is None:
                     continue
 
+                #Ensure that params at index are updated and not as before
                 assert tf.experimental.numpy.allclose(base_param, curve_param)
                 assert not tf.experimental.numpy.allclose(curve_param_old, curve_param)
 
             elif parameter_index != index:
+                #Ensure that all other params remain as before
                 assert tf.experimental.numpy.allclose(curve_param_old, curve_param)
 
 
-
-
-
-        # for base_layer_name, base_layer_param in base_weights.items():
-        #     curve_layer_param= weights[base_layer_name]
-            
-
-
-
-        # for name, weight in weights.items():
-        #     parameter_index = self._find_parameter_index(name)
-
-        # base_name = self._get_base_name(name, index)
-        # base_weight = base_weights.get(base_name)
-
-
-        # curve_layers = initialized_curve_net.curve_layers.layers
-        # for i in range(len(curve_layers)):
-        #     curve_net_kernel= curve_layers[i].curve_kernels[index]
-        #     curve_net_bias= curve_layers[i].curve_biases[index]
-
-        #     #base_model_kernel= base_model.trainable_variables[(2*i)-1]
-        #     #base_model_bias= base_model.trainable_variables[(2*i)]
-
-
-        #     assert tf.experimental.numpy.allclose(curve_net_bias, base_model_bias)
-        #     assert tf.experimental.numpy.allclose(curve_net_kernel, base_model_kernel)
-
+#    def test_init_linear(self, initialized_curve_net):
     def test_init_linear(self):
-        pass
-        #TO DO
+        #curve_net = initialized_curve_net
 
-    def test_get_weighted_parameters(self):
-        pass
-        #TO DO
+        curve_net= CurveNet(
+            num_classes=10,
+            num_bends=3,
+            weight_decay=1e4,
+            curve=Bezier,
+            curve_model=CNN.curve,
+            fix_start=True,
+            fix_end=True,
+            architecture_kwargs=CNN.kwargs,
+        )
+
+        base_model= CNN.base(10, 1e4)
+        base_model.build(input_shape=(None, 28, 28, 1))
+
+        curve_net._build_from_base_model(base_model)
+        index= 0
+        curve_net.import_base_parameters(base_model, index)
+        curve_net.init_linear()
+
+        for layer in curve_net.curve_layers:
+            for curve_param in ["curve_kernels", "curve_biases"]:
+
+            #layer.curve_kernels, 
+                params= getattr(layer, curve_param)
+                first_param, last_param = params[0].value(), params[-1].value()
+                min_param= tf.math.minimum(first_param, last_param)
+                max_param= tf.math.maximum(first_param, last_param)
+
+                for param in params:
+                    # Check elementwise if all params lie in the range of the minimum and maximum param 
+                    # of the start and end model 
+                    assert tf.reduce_all(tf.math.greater_equal(param, min_param))
+                    assert tf.reduce_all(tf.math.less_equal(param, max_param))
+
+
+
+    # def test_get_weighted_parameters(self):
+    #     pass
+    #     #TO DO
 
 
 
 # tempcl= TestCurveNet()
-# tempcl.test_import_base_parameters()
+# tempcl.test_init_linear()
 
 
 
