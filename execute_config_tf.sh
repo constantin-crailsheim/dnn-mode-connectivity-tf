c6ea1b0eb268c1846d1ee5cc5d12891916ce34bc
constantin-crailsheim <constantin@vcrailsheim.de>
2022-08-11 12:16:53 +0000

    Execution file added

---
+++ execute_config_tf.sh
@@ -0,0 +1,26 @@
+# Train and evaluate CNN model
+
+python showcase/scripts/train_tensorflow.py --config cnn-base-model-1
+python showcase/scripts/train_tensorflow.py --config cnn-base-model-2
+python showcase/scripts/train_tensorflow.py --config cnn-curve-pretrained
+python showcase/scripts/train_tensorflow.py --config cnn-curve-resume
+python showcase/scripts/evaluate_tensorflow.py --config cnn-curve-evaluate
+python showcase/scripts/evaluate_tensorflow.py --config cnn-curve-evaluate-point
+
+# Train and evaluate CNNBN model
+
+python showcase/scripts/train_tensorflow.py --config cnnbn-base-model-1
+python showcase/scripts/train_tensorflow.py --config cnnbn-base-model-2
+python showcase/scripts/train_tensorflow.py --config cnnbn-curve-pretrained
+python showcase/scripts/train_tensorflow.py --config cnnbn-curve-resume
+python showcase/scripts/evaluate_tensorflow.py --config cnnbn-curve-evaluate
+python showcase/scripts/evaluate_tensorflow.py --config cnnbn-curve-evaluate-point
+
+# Train and evaluate MLP model
+
+python showcase/scripts/train_tensorflow.py --config mlp-base-model-1
+python showcase/scripts/train_tensorflow.py --config mlp-base-model-2
+python showcase/scripts/train_tensorflow.py --config mlp-curve-pretrained
+python showcase/scripts/train_tensorflow.py --config mlp-curve-resume
+python showcase/scripts/evaluate_tensorflow.py --config mlp-curve-evaluate
+python showcase/scripts/evaluate_tensorflow.py --config mlp-curve-evaluate-point
