d322860c5220fbbc2024490ecdbcb0b3bf60d2b6
constantin-crailsheim <constantin@vcrailsheim.de>
2022-08-12 14:22:23 +0000

    MLP removed from tensorflow training.

--- execute_config_tf.sh
+++ execute_config_tf.sh
@@ -15,12 +15,3 @@ python showcase/scripts/train_tensorflow.py --config cnnbn-curve-pretrained
 python showcase/scripts/train_tensorflow.py --config cnnbn-curve-resume
 python showcase/scripts/evaluate_tensorflow.py --config cnnbn-curve-evaluate-trained
 python showcase/scripts/evaluate_tensorflow.py --config cnnbn-curve-evaluate-point
-
-# Train and evaluate MLP model
-
-python showcase/scripts/train_tensorflow.py --config mlp-base-model-1
-python showcase/scripts/train_tensorflow.py --config mlp-base-model-2
-python showcase/scripts/train_tensorflow.py --config mlp-curve-pretrained
-python showcase/scripts/train_tensorflow.py --config mlp-curve-resume
-python showcase/scripts/evaluate_tensorflow.py --config mlp-curve-evaluate-trained
-python showcase/scripts/evaluate_tensorflow.py --config mlp-curve-evaluate-point
